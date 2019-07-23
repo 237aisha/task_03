6efaca1fc56c5da4f87a865b67dd7f38de739484
237aisha <237aisha@gmail.com>
2019-07-23 13:06:58 +0300

    first-commit

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,39 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
+        'my_list': [
+            {
+                "restaurant_name":"cheese cake factory",
+                "food_type":"international"
+            },
+            {
+                "restaurant_name":"nan",
+                "food_type":"indian"
+            },
+            {
+                "restaurant_name":"krispy",
+                "food_type":"sweet"
+            }
+        ]
+
 
     }
+
+   
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
-
+        'my_object':
+        	{
+                "restaurant_name":"cheese cake factory",
+                "food_type":"international"
+            }
+        
+            
+     
     }
     return render(request, 'detail.html', context)
